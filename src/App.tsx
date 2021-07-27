1ca20be6bdd170cb7bd15e6d19a46610ec401687
Msksgm <msksgm3124@gmail.com>
2021-07-28 07:52:31 +0900

    add: AuthProiverを追加

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,19 @@
 import { FC } from "react";
 import { BrowserRouter, Route, Switch, Redirect } from "react-router-dom";
 
+import { AuthProvider } from "./Auth";
 import Login from "./Login";
 
 const App: FC = () => {
   return (
     <>
-      <BrowserRouter>
-        <Switch>
-          <Route exact path="/login" component={Login} />
-        </Switch>
-      </BrowserRouter>
+      <AuthProvider>
+        <BrowserRouter>
+          <Switch>
+            <Route exact path="/login" component={Login} />
+          </Switch>
+        </BrowserRouter>
+      </AuthProvider>
     </>
   );
 };
