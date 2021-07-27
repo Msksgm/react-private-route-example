dea756e752ce2de8de0d8751afb4d3e15e3a7e79
Msksgm <msksgm3124@gmail.com>
2021-07-28 08:17:39 +0900

    add: PrivateRouteを追加

---
+++ src/PrivateRoute.tsx
@@ -0,0 +1,25 @@
+import { FC, useContext } from "react";
+import { Route, Redirect, RouteProps } from "react-router-dom";
+import { AuthContext } from "./Auth";
+
+import Nav from "./Nav";
+
+const PrivateRoute: FC<RouteProps> = ({ component, ...rest }) => {
+  const { currentUser, loading } = useContext(AuthContext);
+  if (!loading) {
+    if (currentUser) {
+      return (
+        <>
+          <Nav />
+          <Route component={component} {...rest} />;
+        </>
+      );
+    } else {
+      return <Redirect to="/login" />;
+    }
+  } else {
+    return <></>;
+  }
+};
+
+export default PrivateRoute;
