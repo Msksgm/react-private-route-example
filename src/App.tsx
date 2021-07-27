d7cd216f5caf507f570fadcb56ae79312129ec01
Msksgm <msksgm3124@gmail.com>
2021-07-28 07:46:44 +0900

    add: routerを追加

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,19 @@
 import { FC } from "react";
 import logo from "./logo.svg";
 import "./App.css";
+import { BrowserRouter, Route, Switch, Redirect } from "react-router-dom";
+
+import Login from "./Login";
 
 const App: FC = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <BrowserRouter>
+        <Switch>
+          <Route exact path="/login" component={Login} />
+        </Switch>
+      </BrowserRouter>
+    </>
   );
 };
 
