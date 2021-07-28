0510ef37e601d1eb046caa52432604d6ce2d9fcc
Msksgm <msksgm3124@gmail.com>
2021-07-28 21:21:39 +0900

    add: topPageへの遷移とログインしていないときのみ表示するように修正

--- src/LoginPage.tsx
+++ src/LoginPage.tsx
@@ -2,6 +2,7 @@ import { Field, Form, Formik } from "formik";
 import { TextField } from "formik-material-ui";
 import { Fragment, FC, useContext, useEffect } from "react";
 import { AuthContext } from "./Auth";
+import { useHistory } from "react-router-dom";
 
 import {
   Button,
@@ -13,70 +14,84 @@ import {
 
 const LoginPage: FC = (props: any) => {
   const { currentUser, login } = useContext(AuthContext);
+  const history = useHistory();
 
   useEffect(() => {
     currentUser && props.history.push("/top");
   }, [currentUser, props.history]);
 
-  return (
-    <Fragment>
-      <Container>
-        <Grid container>
-          <Grid item md={4}></Grid>
-          <Grid item md={4}>
-            <Formik
-              initialValues={{ username: "", password: "" }}
-              onSubmit={async (value) => {
-                try {
-                  await login(value.username, value.password);
-                  props.history.push("/top");
-                } catch (error) {
-                  alert(error.message);
-                }
-              }}
-              render={({ submitForm, isSubmitting, isValid }) => (
-                <Form>
-                  {isSubmitting && <LinearProgress />}
-                  <FormControl margin="normal" fullWidth>
-                    <Field
-                      style={{ marginTop: "0.5em", marginBottom: "0.5em" }}
-                      name="username"
-                      label="username"
-                      fullWidth
-                      variant="outlined"
-                      component={TextField}
-                    />
-                  </FormControl>
-                  <FormControl fullWidth>
-                    <Field
-                      style={{ marginTop: "0.5em", marginBottom: "0.5em" }}
-                      name="password"
-                      label="Password"
-                      fullWidth
-                      variant="outlined"
-                      type="password"
-                      component={TextField}
-                    />
-                  </FormControl>
-                  <FormControl fullWidth>
-                    <Button
-                      fullWidth
-                      onClick={submitForm}
-                      style={{ marginTop: "0.5em", marginBottom: "0.5em" }}
-                      type="submit"
-                      disabled={!isValid || isSubmitting}
-                    >
-                      Log in
-                    </Button>
-                  </FormControl>
-                </Form>
-              )}
-            />
+  if (!currentUser) {
+    return (
+      <Fragment>
+        <Container>
+          <Grid container>
+            <Grid item md={4}></Grid>
+            <Grid item md={4}>
+              <Formik
+                initialValues={{ username: "", password: "" }}
+                onSubmit={async (value) => {
+                  try {
+                    await login(value.username, value.password);
+                    props.history.push("/top");
+                  } catch (error) {
+                    alert(error.message);
+                  }
+                }}
+                render={({ submitForm, isSubmitting, isValid }) => (
+                  <Form>
+                    {isSubmitting && <LinearProgress />}
+                    <FormControl margin="normal" fullWidth>
+                      <Field
+                        style={{ marginTop: "0.5em", marginBottom: "0.5em" }}
+                        name="username"
+                        label="username"
+                        fullWidth
+                        variant="outlined"
+                        component={TextField}
+                      />
+                    </FormControl>
+                    <FormControl fullWidth>
+                      <Field
+                        style={{ marginTop: "0.5em", marginBottom: "0.5em" }}
+                        name="password"
+                        label="Password"
+                        fullWidth
+                        variant="outlined"
+                        type="password"
+                        component={TextField}
+                      />
+                    </FormControl>
+                    <FormControl fullWidth>
+                      <Button
+                        fullWidth
+                        onClick={submitForm}
+                        style={{ marginTop: "0.5em", marginBottom: "0.5em" }}
+                        type="submit"
+                        disabled={!isValid || isSubmitting}
+                      >
+                        Log in
+                      </Button>
+                    </FormControl>
+                    <FormControl fullWidth>
+                      <Button
+                        color="inherit"
+                        onClick={() => {
+                          history.push("/top");
+                        }}
+                      >
+                        Top(not login)
+                      </Button>
+                    </FormControl>
+                  </Form>
+                )}
+              />
+            </Grid>
           </Grid>
-        </Grid>
-      </Container>
-    </Fragment>
-  );
+        </Container>
+      </Fragment>
+    );
+  }
+  return <></>;
 };
 
 export default LoginPage;
