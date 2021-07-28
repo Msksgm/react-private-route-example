8012b6626175b54c489754c3eab80d2faefc3858
Msksgm <msksgm3124@gmail.com>
2021-07-28 21:01:00 +0900

    add: PrivatePageを追加

---
+++ src/PrivatePage.tsx
@@ -0,0 +1,12 @@
+import { FC } from "react";
+import Container from "@material-ui/core/Container";
+
+const PrivatePage: FC = () => {
+  return (
+    <Container maxWidth="sm">
+      <h1>Private page</h1>
+    </Container>
+  );
+};
+
+export default PrivatePage;
